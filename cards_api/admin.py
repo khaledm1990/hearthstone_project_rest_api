ae9619c6354691c41df8c2764c3f23639f415de1
khaled <khaledm1990.km@gmail.com>
2019-09-24 17:02:30 +0800

    successfully called api/cards API with dumy data

--- cards_api/admin.py
+++ cards_api/admin.py
@@ -2,5 +2,4 @@ from django.contrib import admin
 from cards_api import models
 
 # Register your models here.
-
 admin.site.register(models.Card)
--- cards_api/views.py
+++ cards_api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.views import Response
 # Create your views here.
+
+
+class CardApiView(APIView):
+  """Represent the Crd APIView"""
+  def get(self, request, format=None):
+    """ Returns list of cards cards"""
+    cards = ["cards1", "card2", "card3", "card4"]
+
+    return Response({'success': True, 'cards': cards})
