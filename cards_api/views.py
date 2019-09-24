cca57b059b12f5149de45d50b41d026b9cc496ba
khaled <khaledm1990.km@gmail.com>
2019-09-25 01:02:58 +0800

    move player_class list and sefualt values to constants and retrun empty list is query doesnt match any of the list

--- cards_api/views.py
+++ cards_api/views.py
@@ -4,12 +4,16 @@ from rest_framework import filters
 from cards_api import serializers
 from cards_api import models
 
+DEFAULT_PLAYER_CLASS = 'Neutral'
+PLAYER_CLASS_LIST = ['Druid', 'Hunter', 'Mage', 'Paladin',
+                     'Priest', 'Rogue', 'Shaman', 'Warlock', 'Warrior', 'Neutral']
 
 class CardViewSet(viewsets.ViewSet):
   """Represent the Card ViewSet"""
-
   def list(self, request, format=None):
-    player_class = request.query_params.get('player_class', 'Neutral')[:30]
-    queryset = models.Card.objects.filter(player_class=player_class)
+    player_class = request.query_params.get('player_class')
+    if not player_class in PLAYER_CLASS_LIST:
+      player_class = DEFAULT_PLAYER_CLASS
+    queryset = models.Card.objects.filter(player_class=player_class)[:30]
     serializer = serializers.ListCardSerializer(queryset, many=True)
     return Response(serializer.data)
