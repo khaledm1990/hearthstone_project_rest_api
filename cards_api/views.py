39ece26d795bacbc13eecba50801b93056bcf13e
khaled <khaledm1990.km@gmail.com>
2019-09-26 01:03:27 +0800

    add validation for invalid player_class

--- cards_api/views.py
+++ cards_api/views.py
@@ -12,8 +12,10 @@ class CardViewSet(viewsets.ViewSet):
   """Represent the Card ViewSet"""
   def list(self, request, format=None):
     player_class = request.query_params.get('player_class')
-    if not player_class in PLAYER_CLASS_LIST:
+    if player_class is None:
       player_class = DEFAULT_PLAYER_CLASS
+    if not player_class in PLAYER_CLASS_LIST:
+      return Response({'success': False, 'errors': [{'key': 'player_class', 'message': 'Invalid player_class'}]})
     queryset = models.Card.objects.filter(player_class=player_class)[:30]
     serializer = serializers.ListCardSerializer(queryset, many=True)
     return Response(serializer.data)
