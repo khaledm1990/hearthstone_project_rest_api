41a894b147df5ca5c593974ab6327a818221a1cf
khaled <khaledm1990.km@gmail.com>
2019-09-26 00:41:36 +0800

    update cards migrations

--- cards_api/models.py
+++ cards_api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+# import json
+import requests
 
 class CardManager():
   """Mangaer for user Model """
@@ -9,18 +10,16 @@ class CardManager():
     card.save(using=self._db)
     return card
 class Card(models.Model):
+  TEST = 'test'
   """Database model for cards in the system"""
   card_id = models.CharField(max_length=255, null=False)
   dbf_id = models.CharField(max_length=255, null=False)
   name = models.CharField(max_length=50, null=False)
-  card_set = models.CharField(max_length=500, null=False)
-  type = models.CharField(max_length=50, null=False)
-  text = models.TextField(max_length=255)
   player_class = models.CharField(max_length=50, null=False)
-  locale = models.CharField(max_length=50, null=False)
 
   objects = CardManager()
 
   def __str__(self):
     """Return String representation for our user """
     return self.card_id
+
