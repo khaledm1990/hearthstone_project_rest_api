d8de5e38284a9094035564f901669964388fbd78
khaled <khaledm1990.km@gmail.com>
2019-09-27 13:43:22 +0800

    update requirements.txt file

--- cards_api/views.py
+++ cards_api/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import Response
 from rest_framework import viewsets
 from rest_framework import filters
 from cards_api import serializers
+from django.db.models import Q
 from cards_api import models
 
 DEFAULT_PLAYER_CLASS = 'Neutral'
@@ -12,10 +13,10 @@ class CardViewSet(viewsets.ViewSet):
   """Represent the Card ViewSet"""
   def list(self, request, format=None):
     player_class = request.query_params.get('player_class')
-    if player_class is None:
-      player_class = DEFAULT_PLAYER_CLASS
-    if not player_class in PLAYER_CLASS_LIST:
-      return Response({'success': False, 'errors': [{'key': 'player_class', 'message': 'Invalid player_class'}]})
-    queryset = models.Card.objects.filter(player_class=player_class)[:30]
+    # if player_class is None:
+    #   player_class = DEFAULT_PLAYER_CLASS
+    if not player_class in PLAYER_CLASS_LIST or player_class is None:
+      return Response({'errors': [{'key': 'player_class', 'message': 'Invalid player_class'}]}, status=400)
+    queryset = models.Card.objects.filter(Q(player_class=player_class) | Q(player_class=DEFAULT_PLAYER_CLASS))[:30]
     serializer = serializers.ListCardSerializer(queryset, many=True)
     return Response(serializer.data)
