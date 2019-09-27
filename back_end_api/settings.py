5ee84980be613de72020e7a83f6398968dd0f813
khaled <khaledm1990.km@gmail.com>
2019-09-27 14:09:06 +0800

    added STATIC_ROOT to settings

--- back_end_api/settings.py
+++ back_end_api/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
 	'django.contrib.staticfiles',
 	'rest_framework',
 	'cards_api'
-]
+] 
 
 MIDDLEWARE = [
 	'django.middleware.security.SecurityMiddleware',
@@ -120,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
