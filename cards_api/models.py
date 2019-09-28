687ecb24ea006b024b7aa2c0becd8d47aeeba2b0
khaled <khaledm1990.km@gmail.com>
2019-09-28 21:32:01 +0800

    deployed app to heroku and seed cards sample

--- cards_api/models.py
+++ cards_api/models.py
@@ -21,18 +21,3 @@ class Card(models.Model):
   def __str__(self):
     """Return String representation for our user """
     return self.card_id
-
-  @classmethod
-  def load(cls):
-    resp = requests.get(
-        "https://omgvamp-hearthstone-v1.p.mashape.com/cards/sets/Rastakhan%27s%20Rumble", headers={"X-Mashape-Key": "ZTMJtzbYvXmshPTFEZI4ztIy3I68p1nPwgHjsnIGukKZeJxGcs"})
-    cards = resp.json()
-    for card in cards:
-      print(card)
-      print("========================================")
-      card_id = card.get('cardId')
-      dbf_id = card.get('dbfId')
-      name = card.get('name')
-      player_class = card.get('playerClass', '')
-      cls.objects.create(card_id=card_id, dbf_id=dbf_id, name=name, player_class=player_class)
-
